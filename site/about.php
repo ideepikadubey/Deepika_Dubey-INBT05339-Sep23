bc2235475a7f8fa7ad5f30f115519c84455d22b9
ideepikadubey <121304770+ideepikadubey@users.noreply.github.com>
2023-10-16 14:01:47 +0530

    Add files via upload

---
+++ site/about.php
@@ -0,0 +1,108 @@
+<?php
+require ("includes/common.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>AB Fashion Hub | Shop with us</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Delius Swash Caps' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Andika' rel='stylesheet'>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body style="overflow-x:hidden; padding-bottom:100px; background-color:#D3D3D3;">
+  <?php
+        include 'includes/header_menu.php';
+    ?>
+  <div>
+    <div class="container mt-5 ">
+      <div class="row justify-content-around">
+        <div class="col-md-5 mt-3">
+          <h1 class="text-warning pt-3 title" >Get To Know US</h1>
+          <hr />
+          <img
+            src="https://lh3.googleusercontent.com/p/AF1QipPImLyWrZYUcKPv7_BU4Ik1ZtDY30VYNNxO9Jgc=s1360-w1360-h1020" width = 350px
+            class="img-fluid d-block rounded mx-auto image-thumbnail">
+          <p class="mt-2"><h2>HEY, YOU!<br>
+We're so happy you're here!</h2></p>
+        </div>
+        <div class="col-md-5 mt-3">
+          <span class="text-warning pt-3">
+            <h2 class="title">We Value Our Customers</h2>
+            <h3>Sign up for all the latest collections, sale info, and general musings. But not too many musings; we don't send very many emails.(;</h3>
+
+<h5>(Also, we very much respect your privacy.)</h5>
+          </span>
+          <hr>
+          <p>Located in Kishangarh, Ajmer, AB Fashion Hub is something fancy and Unique— and definitely effective. Simple yet classic, the website is everything you’d expect it to be, including an enjoyable online shopping experience. But, what really elevates it, is its photos, which go beyond the standard professional product photo by including other lifestyle elements, such as freshly picked up lifestyle dressing treands and a place of your choice. While not all of the photos change when you hover, the ones that do definitely add an engaging element, one that makes you want to click to learn more.
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container pb-3">
+  </div>
+  <div class="container mt-3 d-flex justify-content-center card pb-3 col-md-6">
+
+    <form class="col-md-12" action="https://formspree.io/EnterYourEmail" method="POST" name="_next">
+      <h3 class="text-warning pt-3 title mx-auto">Feel Free to Reach US</h3>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email"
+          name="email">
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="5" placeholder="Enter Your Query"></textarea>
+      </div>
+      <input type="hidden" name="_next" value="http://localhost/foody/about.php" />
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
+
+  </div>
+  <!--footer -->
+  <?php include 'includes/footer.php'?>
+  <!--footer end-->
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script>
+  $(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+  });
+  $(document).ready(function () {
+
+    if (window.location.href.indexOf('#login') != -1) {
+      $('#login').modal('show');
+    }
+
+  });
+</script>
+<?php if(isset($_GET['error'])){ $z=$_GET['error']; echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#signup').modal('show');
+});
+</script>"; echo "
+<script type='text/javascript'>alert('".$z."')</script>";} ?>
+<?php if(isset($_GET['errorl'])){ $z=$_GET['errorl']; echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#login').modal('show');
+});
+</script>"; echo "
+<script type='text/javascript'>alert('".$z."')</script>";} ?>
+</html>
---
+++ site/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div  id = "f1" class="text-center"><span class="text-muted"><b>Copyright &copy; All Rights Reserved 2023 | Developer : Deepika Dubey | Contact Us: +91 7737181921
+    </b></span></div>
+</footer>
+    
